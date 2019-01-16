f8ccc2ad1a0615ef0d44d2e7c5364ea4da8ffc48
Illia Klimov <illia.klimov@avira.com>
2019-01-16 14:32:26 +0100

    added fragment

--- AndroidArchitecture/androidmvi/src/main/java/com/example/androidmvi/MainActivity.kt
+++ AndroidArchitecture/androidmvi/src/main/java/com/example/androidmvi/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.androidmvi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    // just to make it simpler
+    private val fragment = MainFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.i("onCreate")
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContainer, fragment)
+            .commit()
     }
 }
